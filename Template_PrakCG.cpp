eeb42b54c4b8091d6aacbd3b2af66ce933d4ddc4
Thoralf Teumer <teut@tuc.onmicrosoft.de>
2022-03-03 11:56:26 +0100

    Hubschrauber Modell bis auf Heckrotor fertig ;)

--- Template_PrakCG.cpp
+++ Template_PrakCG.cpp
@@ -569,13 +569,12 @@ void nase() {
 	glScalef(0.3, 0.25, 0.3);
 	myPartialDisk(0, 2, 0, 360);
 	glPopMatrix();*/
-
 }
 void rumpf() {
 	
 	nase();
 	
-
+	//Unterbau Rumpf
 	glPushMatrix();
 	glTranslatef(1.6, 0.75, 0);
 	glRotatef(-90, 0, 1, 0);
@@ -584,18 +583,164 @@ void rumpf() {
 	myCylinder();
 	glPopMatrix();
 
+	//Wände Rumpf
 	glPushMatrix();
 	glTranslatef(0.1, 0.75, 0.5);
 	glRotatef(-10, 1, 0, 0);
 	glScalef(1.8, 1, 0.1);
 	myCube();
 	glPopMatrix();
-
+	
 	glPushMatrix();
 	glTranslatef(0.1, 0.75, -0.5);
 	glRotatef(180, 0, 1, 0);
 	glRotatef(-10, 1, 0, 0);
 	glScalef(1.8, 1, 0.1);
+	myCube();
+	glPopMatrix();
+
+	/*//Trennwand Cockpit-Rumpf //nicht sichtbar
+	glPushMatrix();
+	glTranslatef(0.95, 0.75, -0.425);
+	glScalef(0.1, 1, 0.85);
+	myCube();
+	glPopMatrix();*/
+
+	//Dach
+	glPushMatrix();
+	glTranslatef(1, 1.75, 0);
+	glScalef(1.8, 0.5, 0.85);
+	glRotatef(-90, 0, 1, 0);
+	myCylinder();
+	glPopMatrix();
+
+	//Cockpit Scheibe
+	glPushMatrix();
+	glTranslatef(1.25, 1.4, 0);
+	glScalef(0.75, 0.75, 0.7);
+	mySphere();
+	glPopMatrix();
+
+	//Rotorenaufsatz
+	glPushMatrix();
+	glTranslatef(-0.3, 1.875, -0.25);
+	glScalef(1, 0.25, 0.5);
+	myCube();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.15, 2.5, 0);
+	glRotatef(90, 1, 0, 0);
+	myCone(0.15, 0.2, 0.5);
+	glPopMatrix();
+
+	static double alpha = 0;
+	alpha += 0.1;
+
+	//Hauptrotoren
+	glPushMatrix();
+	glTranslatef(-0.15, 2.35, 0);
+	glRotatef(alpha, 0, 1, 0);
+		glPushMatrix();
+		glTranslatef(0, 0, -0.05);
+		glRotatef(35, 1, 0, 0);
+		glTranslatef(1.75, 0, 0);
+		glScalef(3.5, 0.025, 0.1);
+		myCube();
+		glPopMatrix();
+	
+	glRotatef(90, 0, 1, 0);
+		glPushMatrix();
+		glTranslatef(0, 0, -0.05);
+		glRotatef(35, 1, 0, 0);
+		glTranslatef(1.75, 0, 0);
+		glScalef(3.5, 0.025, 0.1);
+		myCube();
+		glPopMatrix();
+	
+	glRotatef(90, 0, 1, 0);
+		glPushMatrix();
+		glTranslatef(0, 0, -0.05);
+		glRotatef(35, 1, 0, 0);
+		glTranslatef(1.75, 0, 0);
+		glScalef(3.5, 0.025, 0.1);
+		myCube();
+		glPopMatrix();
+
+	glRotatef(90, 0, 1, 0);
+		glPushMatrix();
+		glTranslatef(0, 0, -0.05);
+		glRotatef(35, 1, 0, 0);
+		glTranslatef(1.75, 0, 0);
+		glScalef(3.5, 0.025, 0.1);
+		myCube();
+		glPopMatrix();
+	glPopMatrix();
+}
+
+void heck() {
+	//Übergang unten
+	glPushMatrix();
+	glScalef(0.5, 0.5, 0.6);
+	mySphere();
+	glPopMatrix();
+
+	//Übergang oben
+	glPushMatrix();
+	glTranslatef(0, 1, 0);
+	glScalef(0.75, 0.25, 0.425);
+	mySphere();
+	glPopMatrix();
+
+	//Verbindungsstück
+	glPushMatrix();
+	glTranslatef(-0.2, 0, 0);
+	glRotatef(-90, 1, 0, 0);
+	glScalef(0.6, 0.8, 1);
+	myCylinder();
+	glPopMatrix();
+
+	//Übergangswände
+	glPushMatrix();
+	glTranslatef(-0.25, 0, 0.5);
+	glRotatef(-10, 1, 0, 0);
+	glTranslatef(0.25, 0, 0.1);
+	glRotatef(-37, 0, 1, 0);
+	glTranslatef(-0.25, 0, -0.1);
+	glScalef(0.5, 1, 0.1);
+	myCube();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-0.25, 0, -0.6);
+	glRotatef(10, 1, 0, 0);
+	glTranslatef(0.25, 0, 0);
+	glRotatef(37, 0, 1, 0);
+	glTranslatef(-0.25, 0, 0);
+	glScalef(0.5, 1, 0.1);
+	myCube();
+	glPopMatrix();
+
+	//heck
+	glPushMatrix();
+	glTranslatef(0, 0.8, 0);
+	glScalef(3, 0.4, 0.28);
+	glRotatef(-90, 0, 1, 0);
+	myCylinder();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-3.1, 0.9, 0);
+	glRotatef(45, 0, 0, 1);
+	glScalef(0.2, 0.5, 0.15);
+	mySphere();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-3.2, 0.575, -0.05);
+	glRotatef(45, 0, 0, 1);
+	glScalef(0.4, 0.85, 0.1);
+	glRotatef(-25, 0, 0, 1);
 	myCube();
 	glPopMatrix();
 }
@@ -609,6 +754,11 @@ void hubschrauber() {
 
 	glPushMatrix();
 	rumpf();
+	glPopMatrix();
+	
+	glPushMatrix();
+	glTranslatef(-0.8, 0.75, 0);
+	heck();
 	glPopMatrix();
 }
 
